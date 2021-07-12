6e1545fd46941f62e9d879cbbda8072c1c04753c
Thiago Trennepohl <thiagotrennepohl@gmail.com>
2021-07-12 12:10:24 +0200

    Remove test code

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/trennepohl/lru-cache-poc/internal/supercache"
-	"github.com/trennepohl/lru-cache-poc/pkg/cache"
-)
-
 //Take a look at the tests :)
 func main(){
-	for i:= 0; i< 200000000; i++ {	c, _ := supercache.New(cache.Config{MaxKeyCount: 100})
-
-		kv := fmt.Sprintf("key%d", i)
-		c.Write(kv,kv)
-	}
 }
